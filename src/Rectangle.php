685585bd7ab64c1c4c769e6325c1a5d0e22b600e
Kellen <sinjorflinchy@gmail.com>
2021-02-05 12:31:37 -0500

    Created Circle Class

--- src/Rectangle.php
+++ src/Rectangle.php
@@ -4,7 +4,7 @@ class Rectangle extends Shape{
 
     public $id;
     public $width;
-    
+
     public function getFullDescription()
     {
         return "Rectangle<" . $this->id . ">:" . $this->name . "-" . $this . $this->length . "X" . $this->width;
